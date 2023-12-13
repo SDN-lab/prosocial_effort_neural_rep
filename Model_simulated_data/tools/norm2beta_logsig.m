69020a2c037c0cbacb93f3401e4d8d70858a5e3d
Jo Cutler <41518104+jocutler@users.noreply.github.com>
2023-12-13 11:49:46 +0000

    Delete Model_simulated_data/tools/norm2beta_logsig.m

--- Model_simulated_data/tools/norm2beta_logsig.m
+++
@@ -1,9 +0,0 @@
-function [ beta_out ] = norm2beta_logsig(beta)
-% transformation from gaussian space to alpha space, as suggested in Daw 2009 Tutorial
-% MKW October 2017
-
-beta_out = logsig(beta)*5;%./2;%+.01;%+.01; %.02 
-beta_out = logsig(beta)*10;%./2;%+.01;%+.01; %.02 
-
-end
-
